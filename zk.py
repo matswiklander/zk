36af74b6ace1df1e18e49af2ad21743b09b47bab
Mats Wiklander <mats.wiklander@gmail.com>
2022-10-26 21:26:29 +0200

    Added requirements

--- zk.py
+++ zk.py
@@ -16,5 +16,12 @@ def new():
     pass
 
 
+@cli.command()
+@click.argument('type', type=click.STRING)
+def add(type: str):
+    click.secho(type, bg='red')
+    pass
+
+
 if __name__ == '__main__':
     cli()
