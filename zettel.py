5f7cda3f498a6859c2e85defbdae2c128788bca0
Mats Wiklander <mats.wiklander@gmail.com>
2022-11-12 22:07:00 +0100

    Cleaned up code

--- zettel.py
+++ zettel.py
@@ -1,24 +1,47 @@
 import os
 import re
 from datetime import datetime
+from os.path import exists
+
+import click
 
 
-def is_taken(zettel_id: str):
+def fetch_all_zettel_file_paths():
     zettel_files = []
     for path, subdirs, files in os.walk(os.getcwd()):
-        for name in files:
-            zettel_files.append(name)
+        for file in files:
+            zettel_files.append(os.sep.join([path, file]))
 
-    regex = re.compile(r'(\d{12})(?:\.md)')
+    regex = re.compile(r'.+?(\d{12})(?:\.md)')
 
     zettel_files = [file for file in zettel_files if regex.search(file)]
 
-    zettel_files = [regex.search(file).group(1) for file in zettel_files if regex.search(file).group(1) == zettel_id]
+    return zettel_files
 
-    if len(zettel_files):
+
+def is_taken(zettel_id: str):
+    zettel_file_paths = fetch_all_zettel_file_paths()
+
+    regex = re.compile(r'.+?(\d{12})(?:\.md)')
+
+    zettel_file_paths = [regex.search(file).group(1) for file in zettel_file_paths if
+                         regex.search(file).group(1) == zettel_id]
+
+    if len(zettel_file_paths):
         return True
 
     return False
+
+
+def initiate_templates_directory():
+    os.makedirs(os.sep.join([os.getcwd(), 'templates']), exist_ok=True)
+
+    for zettel_class in BaseZettel.__subclasses__():
+        template_path = os.sep.join([os.getcwd(), 'templates', zettel_class().mangled_name() + '.md'])
+
+        if not exists(template_path):
+            with open(template_path, 'w', encoding='utf-8') as fp:
+                fp.write(zettel_class().template)
 
 
 class BaseZettel:
@@ -44,12 +67,23 @@ class BaseZettel:
     def __create_id():
         return datetime.now().strftime('%Y%m%d%H%M')
 
+    def __extract_all_tags(self):
+        pass
+
 
 class LinkZettel(BaseZettel):
     template = '''# Link
+
 []()
 
-§link'''
+---
+
+Summary
+
+---
+
+§link
+'''
 
     def __init__(self):
         super().__init__()
@@ -57,8 +91,9 @@ class LinkZettel(BaseZettel):
 
 class JournalZettel(BaseZettel):
     template = '''# Journal
-    
-§journal'''
+
+§journal
+'''
 
     def __init__(self):
         super().__init__()
@@ -66,8 +101,9 @@ class JournalZettel(BaseZettel):
 
 class NoteZettel(BaseZettel):
     template = '''# Note
-    
-§note'''
+
+§note
+'''
 
     def __init__(self):
         super().__init__()
@@ -76,7 +112,8 @@ class NoteZettel(BaseZettel):
 class MinutesZettel(BaseZettel):
     template = '''# Minutes
 
-§minutes'''
+§minutes
+'''
 
     def __init__(self):
         super().__init__()
--- zk.py
+++ zk.py
@@ -2,7 +2,7 @@ import os
 
 import click
 
-from zettel import BaseZettel, is_taken
+from zettel import BaseZettel, is_taken, initiate_templates_directory
 
 
 @click.group()
@@ -13,6 +13,8 @@ def cli():
 @cli.command()
 @click.argument('zettel_type', type=click.STRING)
 def add(zettel_type: str):
+    initiate_templates_directory()
+
     try:
         zettel_class = \
             [zettel_class for zettel_class in BaseZettel.__subclasses__() if
@@ -27,19 +29,11 @@ def add(zettel_type: str):
 
     if not is_taken(zettel.id):
         zettel.create()
+        click.secho(
+            '{} A new {}-zettel has been created'.format(os.sep.join([zettel.mangled_name(), zettel.id + '.md']),
+                                                         zettel.mangled_name()), fg='green')
     else:
         click.secho('You can only create one new zettel every minute.', fg='yellow')
-
-    initiate_templates_directory()
-
-
-def initiate_templates_directory():
-    os.makedirs(os.sep.join([os.getcwd(), 'templates']), exist_ok=True)
-
-    for zettel_class in BaseZettel.__subclasses__():
-        with open(os.sep.join([os.getcwd(), 'templates', zettel_class().mangled_name() + '.md']), 'w',
-                  encoding='utf-8') as fp:
-            fp.write(zettel_class().template)
 
 
 if __name__ == '__main__':
